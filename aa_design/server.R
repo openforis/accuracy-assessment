9aa454fef5cca608a8fc3dad71e27f7fa8c7abcf
Yelena <yfinegold>
2016-12-07 16:24:30 +0100

    fix CEP output

--- aa_design/server.R
+++ aa_design/server.R
@@ -1389,11 +1389,11 @@ shinyServer(
       content = function(file) {
         to_export <- CEfile()
         
-        setwd(paste0(outdir(),"www/cep_template/"))
+        setwd(paste0(outdir(),"/www/cep_template/"))
         zip(zipfile=paste0(outdir(),"/",input$basename_CE,".cep"),
             file=Sys.glob(paste0("*")),
             zip=my_zip_tools)
-        setwd(paste0(outdir(),"../../"))
+        setwd(outdir())
         
         file.copy(paste0(outdir(),"/",input$basename_CE,".cep"), file)
         #file.remove(paste0(outdir(),"/", input$basename_CE,".cep"))
